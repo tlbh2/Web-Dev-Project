8eaa4362e1d17fa66bc2fff36761f11c56cb6f18
tlbh2 <70487263+tlbh2@users.noreply.github.com>
2023-09-10 16:44:17 -0700

    section 18

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,9 +1,69 @@
 var buttonList = document.querySelectorAll(".drum");
-var audio = new Audio("./sounds/tom-1.mp3");
+var crash = new Audio("./sounds/crash.mp3");
+var kickBass = new Audio("./sounds/kick-bass.mp3");
+var snare = new Audio("./sounds/snare.mp3");
+var tom1 = new Audio("./sounds/tom-1.mp3");
+var tom2 = new Audio("./sounds/tom-2.mp3");
+var tom3 = new Audio("./sounds/tom-3.mp3");
+var tom4 = new Audio("./sounds/tom-4.mp3");
 
+
+//add eventListener for each btn
 for (var i = 0; i < buttonList.length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        this.style.color = "white";
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
+//add eventListener to all document but only call the callback function when the typeOfEvent is match up, in this case "keydown" key on keyboard
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+})
+
+
+function makeSound(key){
+    switch (key){
+        case "w":
+            
+            crash.play();
+            break;
+        case "a":
+            
+            kickBass.play();
+            break;
+        case "s":
+            
+            snare.play();
+            break;
+        case "d":
+            
+            tom1.play();
+            break;
+        case "j":
+            
+            tom2.play();
+            break;
+        case "k":  
+            
+            tom3.play();
+            break; 
+        case "l": 
+            
+            tom4.play();
+            break;
+        default: console.log(buttonInnerHTML);
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.toggle("pressed");
+    setTimeout(function(){
+        activeButton.classList.toggle("pressed");
+    }, 200);
+}
